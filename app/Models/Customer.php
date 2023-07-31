952836673117b19fec1a31f7962785cc37a05ce8
puikepixels <bram@puikepixels.com>
2023-07-31 23:08:50 +0200

    implement Spatie Log

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,9 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Customer extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logUnguarded();
+    }
 }
