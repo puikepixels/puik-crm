22f2de32f40f2eab5b6a22493f306880f76a8ef0
puikepixels <bram@puikepixels.com>
2023-07-31 23:24:13 +0200

    Add Customergroup model

---
+++ app/Http/Controllers/CustomerGroupController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCustomerGroupRequest;
+use App\Http\Requests\UpdateCustomerGroupRequest;
+use App\Models\CustomerGroup;
+
+class CustomerGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCustomerGroupRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomerGroup $customerGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomerGroup $customerGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCustomerGroupRequest $request, CustomerGroup $customerGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomerGroup $customerGroup)
+    {
+        //
+    }
+}
