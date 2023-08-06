bef8df3d0c5a2beaa8d39db1b750d76226b5ae89
puikepixels <bram@puikepixels.com>
2023-08-06 15:32:45 +0200

    Add toastr notification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,34 @@
 import './bootstrap';
+import toastr from 'toastr';
+
 
 Livewire.start();
+
+
+toastr.options = {
+  "closeButton": true,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "100",
+  "hideDuration": "1000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+
+Livewire.on('alert', ({ type, message }) => {
+  toastr[type](message);
+
+})
+
+
+// window.livewire.on('alert', param => {
+//   toastr[param['type']](param['message']);
+// });
+
 
